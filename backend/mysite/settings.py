9a65d4a08c645a5b7efba7d7ef8861ede6f2d53e
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2025-12-23 17:32:02 +0545

    main view app

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'von_lipwig'
 ]
 
 MIDDLEWARE = [
---
+++ backend/von_lipwig/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VonLipwigConfig(AppConfig):
+    name = 'von_lipwig'
