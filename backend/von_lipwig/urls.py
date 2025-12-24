7f18dd051703d0a78982f4df173dabdefbfaafdc
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2025-12-24 16:10:41 +0545

    extract input values from the form

--- backend/von_lipwig/urls.py
+++ backend/von_lipwig/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), #if the URL is von_lipwig/ then call views.home and take the user to the home page
+    path('send_msg', views.send_msg, name='send_msg'), #if the URL is von_lipwig/send_msg then call views.send_msg to process the message sending
 ]
--- backend/von_lipwig/views.py
+++ backend/von_lipwig/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def send_msg(request):
+    if request.method == 'POST':
+        user_email = request.POST.get('mail') # gets the value of the input field with name 'mail'
+        recipientsList = request.POST.getlist('recipients')
+        message = request.POST.get('message')
+        print(f"User Email: {user_email}")
+        print(f"Recipients: {recipientsList}")
+        print(f"Message Content: {message}")
+    return render(request, 'home.html')
