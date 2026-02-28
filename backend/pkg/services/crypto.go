d376e9e98066c7b76f9ed223a6ccd2f27364c0f3
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-02-28 18:43:49 +0545

    hash func

--- backend/pkg/services/crypto.go
+++ backend/pkg/services/crypto.go
@@ -1,8 +1,19 @@
 package services
 
-//encrypt the messages, attachments, access pairs before storing in the db
+import (
+	"database/sql"
 
-func EncryptStr(str string) string {
-	//use a hash func here
-	return str
+	"golang.org/x/crypto/bcrypt"
+)
+
+// encrypt the messages, attachments, access pairs before storing in the db
+const salt = 13
+
+func HashIns(str string, db *sql.DB) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(str), salt)
+	if err != nil {
+		logger.Error("Error while hashing", "error", err)
+		return "", err
+	}
+	return string(hash), nil
 }
