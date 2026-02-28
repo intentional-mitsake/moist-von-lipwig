95e5d72032d7ad1baff6033d20a17483bcedc408
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-02-28 19:13:10 +0545

    emali valid/verif

--- backend/pkg/services/postmaster.go
+++ backend/pkg/services/postmaster.go
@@ -1,5 +1,10 @@
 package services
 
+import (
+	"net"
+	"strings"
+)
+
 //check for delivery dates from the DB every 3 days
 //this will vastly reduce the load on the server
 //there is a risk of some posts being missed which are posted after the last check
@@ -12,3 +17,13 @@ package services
 func CheckDeliveryDates() {}
 
 func SendEmails() {}
+
+func DomainExists(email string) (bool, error) {
+	_, domain := splitEmail(email)
+	mx, err := net.LookupMX(domain) //chechks if teh part after @ is a valid domain
+	return err == nil && len(mx) > 0, err
+}
+func splitEmail(email string) (string, string) {
+	parts := strings.Split(email, "@")
+	return parts[0], parts[1]
+}
